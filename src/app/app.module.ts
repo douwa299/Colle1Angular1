4e48670b0b3dbe1f3f37edad3069e413ecfe6933
douwa299 <douaaberrahmo@gmail.com>
2024-12-10 14:42:02 +0100

    fifth Commit
    11-10-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { RegistreComponent } from './registre/registre.component';
+import { PanierComponent } from './panier/panier.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RegistreComponent,
+    PanierComponent,
+    ProduitsComponent,
+    HeaderComponent,
+    FooterComponent
+ 
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
